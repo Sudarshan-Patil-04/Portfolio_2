a721efe58f838963ea5de0b318b99ce0b032797f
Sudarshan Patil <patilsudarshan9975@gmail.com>
2026-02-16 21:40:09 +0530

    Fixed base path for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', 
+  base: '/',   // ✅ FIXED
   plugins: [react()],
   server: {
     hmr: {
